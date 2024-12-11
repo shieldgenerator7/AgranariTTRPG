fdfc456d0fadb4f077893399c64f3624d54b84e2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-11 02:49:52 -0500

    Update Attribute.js

--- Software/react-canvas-draw/src/Data/Attribute.js
+++ Software/react-canvas-draw/src/Data/Attribute.js
@@ -3,76 +3,79 @@
 import { clamp } from "../Utility/Utility";
 import { LIMIT_POSITIVE_ONLY, ONCLICK_ADJUST_VALUE, ONCLICK_DIE_ROLL, ONCLICK_TOGGLE, REGEX_SPACER_TEST } from "./Constants";
 
+let stringifyAttribute = [
+    "name",
+    "displayname",
+    "xp",
+    "xp_variance",
+]
+
 class Attribute {
-    constructor(name, value = 0, max = 0, dieRoll = "1d20") {
+    constructor(name, statCost) {
+        
+        //save variables
         this.name = name;
         this.displayName = "";
-        this.value = value;
-        this.max = max;
-        this.onclick = ONCLICK_ADJUST_VALUE;
-        this.limit = LIMIT_POSITIVE_ONLY;
-        this.dieRoll = dieRoll;//"d20", "2d8"
-        this.visible = true;
+        this.xp = 0;
+        this.xp_variance = 0;
+
+        //computed variables
+        this.statCost = statCost ?? 20;
+        this.bonusList = {};
+        this.bonusTotal = 0;
+        this.lastRoll = 0;
+        this.statValue = 0;
+        this.statVarianceValue = 0;
     }
 
-    get Value() {
-        return this.value;
-    }
-    set Value(value) {
-        if (this.max > 0) {
-            this.value = clamp(value, 0, this.max);
-        }
-        else {
-            this.value = value;
-        }
+    get Stat() {
+        return this.statValue;
     }
 
-    get OnClickType() {
-        if (this.max > 0) {
-            return ONCLICK_ADJUST_VALUE;
-        }
-        if (this.dieRoll) {
-            return ONCLICK_DIE_ROLL;
-        }
-        return ONCLICK_ADJUST_VALUE;
+    get StatVariance() {
+        return this.statVarianceValue;
+    }
+
+    acceptStatCost(statCost) {
+        this.statCost = statCost;
+        this._computeValues();
+    }
+
+    acceptBonus(source, bonus) {
+        this.bonusList[source] = bonus;
+        this._computeValues();
+    }
+
+    removeBonus(source) {
+        delete this.bonusList[source];
+        this._computeValues();
+    }
+
+    _computeValues() {
+        //bonus total        
+        this.bonusTotal = 0;
+        this.bonusList ??= {};
+        Object.entries(this.bonusList).forEach(([k, v]) => {
+            this.bonusTotal += v; 
+        });
+
+        //stat value
+        this.statValue = this.xp / this.statCost + this.bonusTotal;
+
+        //stat variance value
+        this.statVarianceValue = this.xp_variance / (this.statCost / 5);
     }
 
     getDisplayText() {
-        const name = this.displayName || this.name;
-        const value = (this.value || 0) * 1;
-        //type: limited resource
-        switch (this.OnClickType) {
-            case ONCLICK_ADJUST_VALUE:
-                if (this.max > 0) {
-                    return `${name}: ${value}/${this.max}`;
-                }
-                else {
-                    return `${name}: ${value}`;
-                }
-                break;
-            case ONCLICK_DIE_ROLL:
-                let valueModStr = ((value > 0) ? "+" : "") + value;
-                if (this.dieRoll == "1d20" || this.dieRoll == "d20") {
-                    return `${name}: ${valueModStr}`;
-                }
-                if (value != 0) {
-                    return `${name}: ${this.dieRoll} ${valueModStr}`;
-                }
-                else {
-                    return `${name}: ${this.dieRoll}`;
-                }
-                break;
-            default:
-                return `${name}`;
-        }
-    }
-
-    get IsSpacer() {
-        return REGEX_SPACER_TEST.test(this.name);
+        return `${this.displayName ?? this.name}: ${this.Stat} - ${this.StatVariance} = ${this.lastRoll || "---"}`;
     }
 }
 export default Attribute;
 
 export function inflateAttribute(attribute) {
     Object.setPrototypeOf(attribute, Attribute.prototype);
+
+    attribute.statCost = attribute.statCost ?? 20;
+    attribute.lastRoll = 0;
+    attribute._computeValues();
 }

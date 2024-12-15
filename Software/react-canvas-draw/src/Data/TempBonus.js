0448c89387b22fc723c6cbae9fbba02348db439e
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-15 18:31:14 -0500

    Add attr: TempBonus.source

--- Software/react-canvas-draw/src/Data/TempBonus.js
+++ Software/react-canvas-draw/src/Data/TempBonus.js
@@ -1,9 +1,10 @@
 "use strict";
 
 class TempBonus {
-    constructor(bonus, filter) {
-        this.bonus = bonus;
-        this.filter = filter;
+    constructor(bonus, filter, source) {
+        this.bonus = bonus;//the bonus amount
+        this.filter = filter;//what it is a bonus to (ex: Accuracy, Dodge Variance, etc)
+        this.source = source;//where the bonus is coming from (ex: species, feature, etc)
 
         this.editing = false;
     }

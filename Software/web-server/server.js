0fd19efde0de4a7be851dd60f62dfc97b7d87703
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-24 22:28:52 -0500

    Merge app.js into server.js

--- Software/web-server/server.js
+++ Software/web-server/server.js
@@ -1,9 +1,31 @@
-"use strict"
+"use strict";
 //2019-04-04: copied from https://stackoverflow.com/a/41550669/2336212
+//2024-12-13: created by following tutorial: https://youtu.be/HXquxWtE5vA?si=nkCgddL8dMa5fmEF
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+//socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const port = 3001;
+
+app.use(express.static('public'));
 app.use(express.static(__dirname + '/')); //__dir and not _dir
-var port = 8000; // you can use any port
-app.listen(port);
-console.log('server on ' + port);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected', socket);
+})
+
+server.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+console.log('server loaded!');

78d1272540f8eff737ce6d772086ac573f692c03
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-24 21:36:05 -0500

    Try to fix config problem

--- Software/react-canvas-draw/config-overrides.js
+++ Software/react-canvas-draw/config-overrides.js
@@ -20,5 +20,27 @@ module.exports = function override (config, env) {
         "timers": require.resolve("timers-browserify"),
     }
     
+    // //2024-12-21: copied from https://stackoverflow.com/questions/30239060/uncaught-referenceerror-process-is-not-defined
+    // const webpack = require('webpack');
+    // const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
+    // const isDevelopment = process.env.NODE_ENV !== 'production';
+    // config.plugins.push(
+    //     new webpack.DefinePlugin({
+    //         'process.env': JSON.stringify(dotenv.parsed),
+    //         'process.env.NODE_ENV': JSON.stringify(isDevelopment ? 'development' : 'production'),
+    //     })
+    // );
+
+    //2024-12-21: copied from https://stackoverflow.com/a/71205013/2336212
+    const webpack = require('webpack');
+    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+    config.plugins = [
+        ...config.plugins,
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ];
+    
     return config
 }

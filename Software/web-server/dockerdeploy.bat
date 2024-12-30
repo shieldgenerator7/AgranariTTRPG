a61748a624eab8194d34686ee6314d9920497543
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-12-30 03:13:20 -0500

    Update deploy instructions

--- Software/web-server/dockerdeploy.bat
+++ Software/web-server/dockerdeploy.bat
@@ -9,3 +9,35 @@ docker push shieldgenerator7/agranari
 :: https://hub.docker.com/r/shieldgenerator7/agranari/tags
 
 :: https://console.cloud.google.com/run/detail/us-central1/agranari/revisions?project=creaturecombat
+
+
+
+
+:: DEPLOY INSTRUCTIONS
+
+
+
+
+:: CLIENT
+:: make sure you push all your changes on main
+:: switch to gh-pages branch in github
+:: pull all changes from main into gh-pages
+:: run deployFull.bat
+:: copy build folder contents into game folder in the top level repository folder
+:: commit those changes, push those commits
+:: > Client Done!
+
+
+
+::SERVER
+:: Make sure you have docker hub running
+:: run the deployFull bat first
+:: run this bat file, wait for it to finish
+:: go to the google cloud page and make a new revision: "edit & deploy new revision"
+:: wait for that to finish
+:: > Server done!
+
+
+
+:: user link:
+:: https://shieldgenerator7.github.io/AgranariTTRPG/game/?url=https://latest---agranari-zxrggs7xhq-uc.a.run.app/

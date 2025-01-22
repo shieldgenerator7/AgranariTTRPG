689a6ed39f0c5adf6e3f6d50454e0f44324538c5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-22 18:19:29 -0500

    TEMP Copy storage.js into server.js*
    *just to get it to work, they are meant to be separate files, but for now, they are one file

--- Software/web-server/server.js
+++ Software/web-server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const { storeCharacter, getCharacterNameList, loadCharacter } = require('./storage');
+// const { storeCharacter, getCharacterNameList, loadCharacter } = require('./storage');
 const io = new Server(
     server,
     {
@@ -126,3 +126,75 @@ console.log('server loaded!');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// 2025-01-22: this section copied from storage.js
+// theyre meant to be two separate files, but the module thing wasnt working
+// putting the storage.js functions here made it work, so that's what i did (for now)
+//
+//
+
+//2025-01-21: copied from https://stackoverflow.com/a/77181535/2336212
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+
+const REGION = "us-east-1";
+
+let s3 = new S3Client({ region: REGION });
+
+const BUCKET_NAME = "agranari-characters";
+
+
+
+function getCharacterNameList() {
+    return [];
+}
+
+async function storeCharacter(character) {
+    //2025-01-21: copied from https://stackoverflow.com/a/77181535/2336212
+    const params = getParams(character);
+    // Create an object and upload it to the Amazon S3 bucket.
+    try {
+        const results = await s3.send(new PutObjectCommand(params));
+        console.log(
+            "Successfully created " +
+            params.Key +
+            " and uploaded it to " +
+            params.Bucket +
+            "/" +
+            params.Key
+        );
+        return results; // For unit tests.
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+function loadCharacter(characterName) {
+    return {};
+}
+
+function getKey(characterName) {
+    return `${characterName}.json`;
+}
+function getParams(character) {
+    return {
+        Bucket: BUCKET_NAME,
+        Key: getKey(character.name),
+        Body: JSON.stringify(character),
+    };
+}

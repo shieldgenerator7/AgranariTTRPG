3cf40ffd1af640e2fb7bf67094602de3d8d454f8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2025-01-22 16:31:05 -0500

    Implement storeCharacter()

--- Software/web-server/storage.js
+++ Software/web-server/storage.js
@@ -1,14 +1,56 @@
 
+//2025-01-21: copied from https://stackoverflow.com/a/77181535/2336212
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const REGION = "us-east-1";
+
+const s3 = new S3Client({ region: REGION });
+
+const BUCKET_NAME = "agranari-characters";
+
+// Set the parameters
+const params = {
+    Bucket: "agranari-characters",
+    Key: "",
+    Body: {},
+};
 
 
 export function getCharacterNameList() {
     return [];
 }
 
-export function storeCharacter(character) {
-    
+export async function storeCharacter(character) {
+    //2025-01-21: copied from https://stackoverflow.com/a/77181535/2336212
+    const params = getParams(character);
+    // Create an object and upload it to the Amazon S3 bucket.
+    try {
+        const results = await s3.send(new PutObjectCommand(params));
+        console.log(
+            "Successfully created " +
+            params.Key +
+            " and uploaded it to " +
+            params.Bucket +
+            "/" +
+            params.Key
+        );
+        return results; // For unit tests.
+    } catch (err) {
+        console.log("Error", err);
+    }
 }
 
 export function loadCharacter(characterName) {
     return {};
+}
+
+function getKey(characterName) {
+    return `${characterName}.json`;
+}
+function getParams(character) {
+    return {
+        Bucket: BUCKET_NAME,
+        Key: getKey(character.name),
+        Body: JSON.stringify(character),
+    };
 }

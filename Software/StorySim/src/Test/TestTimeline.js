b48460cfce04cd7cae90580f204c19c42b3230b4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2026-04-09 23:44:45 -0400

    Test with timeline and events

--- Software/StorySim/src/Test/TestTimeline.js
+++ Software/StorySim/src/Test/TestTimeline.js
@@ -1,24 +1,39 @@
-import Character from "../Data/Character";
+import Character, { inflateCharacter } from "../Data/Character";
 import Stat from "../Data/Stat";
+import Vector2 from "../Data/Vector2";
+import { Event } from "../System/Event";
+import { Scene } from "../System/Scene";
 import { copy } from "../Utility/Utility";
 
 
 export function testTimeline (){
     console.log("test timeline");
 
-    //Setup
+    //Setup: Character
     const char1 = new Character("test");
     const stat = new Stat("Strength", 20);
     stat.XP = 100;
     stat.XPVariance = 250;
     char1.statList.push(stat);
 
+    //Setup: Scene
+    const scene = new Scene(0, new Vector2(0, 0));
+    scene.characterList.push(char1);
+
+    //Setup: Timeline
+    const timeline = scene.timeline;
+    const event = new Event(1, "test", "buff");
+    event.addDiff(0, "Strength", 2);
+    timeline.eventList.push(event);
+    console.log("scene", copy(scene));
+
     //Pre Output
-    console.log(copy(char1), char1.statList[0].Stat);
+    console.log("Char1", copy(char1), char1.statList[0].Stat);
 
     //Change
-    stat.XP = 145;
+    const char2 = timeline.fastForward(char1, 2);
+    inflateCharacter(char2);
 
     //Post Output
-    console.log(copy(char1), char1.statList[0].Stat);
+    console.log("Char2", copy(char2), char2.statList[0].Stat);
 };

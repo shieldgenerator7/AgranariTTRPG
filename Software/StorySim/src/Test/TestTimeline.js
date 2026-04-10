bdc3812b1843cdfbb9252321c6c4d100935e63c4
shieldgenerator7 <shieldgenerator7@yahoo.com>
2026-04-09 22:51:46 -0400

    First version of test

--- Software/StorySim/src/Test/TestTimeline.js
+++ Software/StorySim/src/Test/TestTimeline.js
@@ -1,5 +1,24 @@
+import Character from "../Data/Character";
+import Stat from "../Data/Stat";
+import { copy } from "../Utility/Utility";
 
 
 export function testTimeline (){
     console.log("test timeline");
+
+    //Setup
+    const char1 = new Character("test");
+    const stat = new Stat("Strength", 20);
+    stat.XP = 100;
+    stat.XPVariance = 250;
+    char1.statList.push(stat);
+
+    //Pre Output
+    console.log(copy(char1), char1.statList[0].Stat);
+
+    //Change
+    stat.XP = 145;
+
+    //Post Output
+    console.log(copy(char1), char1.statList[0].Stat);
 };
